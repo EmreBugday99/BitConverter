8b45c734b035c09cfe1b52e88e43248e8dffe17a
Emre Bugday <emreb25@outlook.com>
2021-08-29 01:38:55 +0300

    Implemented long int, long long int, float, double

--- BitConverter/BitConverter.c
+++ BitConverter/BitConverter.c
@@ -1,9 +1,53 @@
 #include "BitConverter.h"
+#include <stdio.h>
+#include <string.h>
 
-void GetBytesInt32(int data, unsigned char* buffer)
+void GetBytesInt32(int data, unsigned char *buffer)
 {
-    for (unsigned int i = 0; i < sizeof(int); i++)
+    intUnion.i = data;
+
+    for (int i = 0; i < sizeof(int); i++)
     {
-        buffer[i] = data >> i * 8;
+        buffer[i] = intUnion.c[i];
+    }
+}
+
+void GetBytesLongInt32(long int data, unsigned char *buffer)
+{
+    longIntUnion.i = data;
+
+    for (int i = 0; i < sizeof(long int); i++)
+    {
+        buffer[i] = longIntUnion.c[i];
+    }
+}
+
+void GetBytesLongLongInt32(long long int data, unsigned char *buffer)
+{
+    longLongIntUnion.i = data;
+
+    for (int i = 0; i < sizeof(long long int); i++)
+    {
+        buffer[i] = longLongIntUnion.c[i];
+    }
+}
+
+void GetBytesFloat(float data, unsigned char *buffer)
+{
+    floatUnion.f = data;
+
+    for (int i = 0; i < sizeof(float); i++)
+    {
+        buffer[i] = floatUnion.c[i];
+    }
+}
+
+void GetBytesDouble(double data, unsigned char *buffer)
+{
+    doubleUnion.d = data;
+
+    for (int i = 0; i < sizeof(double); i++)
+    {
+        buffer[i] = doubleUnion.c[i];
     }
 }
--- BitConverter/BitConverter.h
+++ BitConverter/BitConverter.h
@@ -1,6 +1,40 @@
 #ifndef BITCONVERTER
 
-void GetBytesInt32(int data, unsigned char* buffer);
+union
+{
+    int i;
+    unsigned char c[sizeof(int)];
+} intUnion;
+
+union
+{
+    long int i;
+    unsigned char c[sizeof(long int)];
+} longIntUnion;
+
+union
+{
+    long long int i;
+    unsigned char c[sizeof(long long int)];
+} longLongIntUnion;
+
+union
+{
+    float f;
+    unsigned char c[sizeof(float)];
+} floatUnion;
+
+union
+{
+    double d;
+    unsigned char c[sizeof(double)];
+} doubleUnion;
+
+void GetBytesInt32(int data, unsigned char *buffer);
+void GetBytesLongInt32(long int data, unsigned char *buffer);
+void GetBytesLongLongInt32(long long int data, unsigned char *buffer);
+void GetBytesFloat(float data, unsigned char *buffer);
+void GetBytesDouble(double data, unsigned char *buffer);
 
 #define BITCONVERTER
 #endif
